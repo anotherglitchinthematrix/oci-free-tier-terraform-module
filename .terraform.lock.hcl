2d7aec0b97b2ab3a1657e56e04678b5f197a7474
Ali Can YILDIZ <anotherglitchinthematrix@gmail.com>
2021-12-08 13:53:14 +0300

    Main

---
+++ .terraform.lock.hcl
@@ -0,0 +1,37 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/null" {
+  version = "3.1.0"
+  hashes = [
+    "h1:xhbHC6in3nQryvTQBWKxebi3inG5OCgHgc4fRxL0ymc=",
+    "zh:02a1675fd8de126a00460942aaae242e65ca3380b5bb192e8773ef3da9073fd2",
+    "zh:53e30545ff8926a8e30ad30648991ca8b93b6fa496272cd23b26763c8ee84515",
+    "zh:5f9200bf708913621d0f6514179d89700e9aa3097c77dac730e8ba6e5901d521",
+    "zh:9ebf4d9704faba06b3ec7242c773c0fbfe12d62db7d00356d4f55385fc69bfb2",
+    "zh:a6576c81adc70326e4e1c999c04ad9ca37113a6e925aefab4765e5a5198efa7e",
+    "zh:a8a42d13346347aff6c63a37cda9b2c6aa5cc384a55b2fe6d6adfa390e609c53",
+    "zh:c797744d08a5307d50210e0454f91ca4d1c7621c68740441cf4579390452321d",
+    "zh:cecb6a304046df34c11229f20a80b24b1603960b794d68361a67c5efe58e62b8",
+    "zh:e1371aa1e502000d9974cfaff5be4cfa02f47b17400005a16f14d2ef30dc2a70",
+    "zh:fc39cc1fe71234a0b0369d5c5c7f876c71b956d23d7d6f518289737a001ba69b",
+    "zh:fea4227271ebf7d9e2b61b89ce2328c7262acd9fd190e1fd6d15a591abfa848e",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/oci" {
+  version = "4.57.0"
+  hashes = [
+    "h1:uPE7YvZj1CB4cGY1xWYkB51AtSOaoNMaFM/kHgw0f8c=",
+    "zh:04037e0a5e59bf0f5c63939a63cd2ae902ba42110561e18a5e115d32dbbc6490",
+    "zh:061d32086b861859f8650903806f051d4163227cf86877904cd6292662b715c7",
+    "zh:3763c2ef6c1022df765f0d254d883c540ec96e0bd4761c897480359e46a8e560",
+    "zh:64872eee45a1ca7c2b63f5cc898cd5ded1e208b09d29642514e8f8948754d913",
+    "zh:66c6b280992e73d9fe7cd91dab7a1d1567f3b58f6fcda23049e6f506b58fc252",
+    "zh:7ca650a9d47564c11d301925d60c1ec48a1c541c6f016ebee3631434f718ad03",
+    "zh:91debc579b4356d49e795bd7ce0a59babdc3d501557f6824e9800f8195063121",
+    "zh:9781474b13fdd755177ab612173f8629fb96cded4f3992c7a7a91855fff22476",
+    "zh:a626626519fde75e3a9ad8c2db2294b1fcc8bbe5ca72c50da78182c3c79cf4eb",
+    "zh:f6c3c6397660d0b0537457dce465a9f6b6a95a5b1f6c0988d1996d115ebc835c",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,154 @@
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+
+resource "oci_core_instance" "atlas_instance" {
+  count               = var.num_instances
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  compartment_id      = var.compartment_ocid
+  display_name        = format("%s${count.index}", replace(title(var.instance_name), "/\\s/", ""))
+  shape               = var.instance_shape
+
+  shape_config {
+    ocpus         = var.instance_ocpus
+    memory_in_gbs = var.instance_shape_config_memory_in_gbs
+  }
+
+  create_vnic_details {
+    subnet_id                 = oci_core_subnet.atlas_subnet.id
+    display_name              = format("%sVNIC", replace(title(var.instance_name), "/\\s/", ""))
+    assign_public_ip          = true
+    assign_private_dns_record = true
+    hostname_label            = format("%s${count.index}", lower(replace(var.instance_name, "/\\s/", "")))
+  }
+
+  source_details {
+    source_type = var.instance_source_type
+    source_id   = var.instance_image_ocid[var.region]
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_keys
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
+
+data "oci_core_instance_devices" "atlas_instance_devices" {
+  count       = var.num_instances
+  instance_id = oci_core_instance.atlas_instance[count.index].id
+}
+
+output "instance_private_ips" {
+  value = [oci_core_instance.atlas_instance.*.private_ip]
+}
+
+output "instance_public_ips" {
+  value = [oci_core_instance.atlas_instance.*.public_ip]
+}
+
+output "boot_volume_ids" {
+  value = [oci_core_instance.atlas_instance.*.boot_volume_id]
+}
+
+output "instance_devices" {
+  value = [data.oci_core_instance_devices.atlas_instance_devices.*.devices]
+}
+
+resource "oci_core_vcn" "atlas_vcn" {
+  cidr_block     = "10.1.0.0/16"
+  compartment_id = var.compartment_ocid
+  display_name   = format("%sVCN", replace(title(var.instance_name), "/\\s/", ""))
+  dns_label      = format("%svcn", lower(replace(var.instance_name, "/\\s/", "")))
+}
+
+resource "oci_core_security_list" "atlas_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.atlas_vcn.id
+  display_name   = format("%sSecurityList", replace(title(var.instance_name), "/\\s/", ""))
+
+  # Allow outbound tcp traffic on all ports
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol    = "6"
+    stateless   = false
+  }
+
+  # Allow inbound tcp traffic on all ports
+  ingress_security_rules {
+    protocol  = "6"
+    source    = "0.0.0.0/0"
+    stateless = false
+  }
+
+  # Allow inbound icmp traffic of a specific type
+  ingress_security_rules {
+    protocol  = 1
+    source    = "0.0.0.0/0"
+    stateless = false
+
+    icmp_options {
+      type = 3
+      code = 4
+    }
+  }
+}
+
+resource "oci_core_internet_gateway" "atlas_internet_gateway" {
+  compartment_id = var.compartment_ocid
+  display_name   = format("%sIGW", replace(title(var.instance_name), "/\\s/", ""))
+  vcn_id         = oci_core_vcn.atlas_vcn.id
+}
+
+resource "oci_core_default_route_table" "default_route_table" {
+  manage_default_resource_id = oci_core_vcn.atlas_vcn.default_route_table_id
+  display_name               = "DefaultRouteTable"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.atlas_internet_gateway.id
+  }
+}
+
+resource "oci_core_subnet" "atlas_subnet" {
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  cidr_block          = "10.1.20.0/24"
+  display_name        = format("%sSubnet", replace(title(var.instance_name), "/\\s/", ""))
+  dns_label           = format("%ssubnet", lower(replace(var.instance_name, "/\\s/", "")))
+  security_list_ids   = [oci_core_security_list.atlas_security_list.id]
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_vcn.atlas_vcn.id
+  route_table_id      = oci_core_vcn.atlas_vcn.default_route_table_id
+  dhcp_options_id     = oci_core_vcn.atlas_vcn.default_dhcp_options_id
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.tenancy_ocid
+  ad_number      = var.availability_domain
+}
+
+resource "null_resource" "remote-exec" {
+  count = var.auto_iptables ? var.num_instances : 0
+
+  connection {
+    agent       = false
+    timeout     = "30m"
+    host        = element(oci_core_instance.atlas_instance.*.public_ip, count.index)
+    user        = "ubuntu"
+    private_key = file(var.ssh_private_key)
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export DATE=$(date +%Y%m%d); sudo iptables -L > \"/home/ubuntu/iptables-$DATE.bak\"",
+      "sudo sh -c 'iptables -D INPUT -j REJECT --reject-with icmp-host-prohibited 2> /dev/null; iptables-save > /etc/iptables/rules.v4;'",
+    ]
+  }
+}
